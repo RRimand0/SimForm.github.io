8833cd858cea859c9623ff692b2312ce4b42bb55
RRimand0 <ricardorimando22@gmail.com>
2023-05-09 21:55:01 +0800

    second update

--- script.js
+++ script.js
@@ -14,33 +14,74 @@ const insert = () => {
 
 let id= 0
 const appendText = (label = "No label", placeholder = "No placeholder") => {
-let wrap= document.createElement('div')
+    let wrap= document.createElement('div')
     wrap.setAttribute('id', id)
-let del = document.createElement('button')
+
+    let del = document.createElement('button')
     del.setAttribute('onclick', 'document.getElementById("'+id+'").style.display="none"')
     del.textContent="delete"
-let el = document.createElement('input') 
+
+    let el = document.createElement('input') 
     el.setAttribute('class', 'mx-1 mt-1') 
     el.setAttribute('placeholder', placeholder) 
     el.setAttribute('type', 'text')
-let lbl = document.createElement('label') 
+
+    let lbl = document.createElement('label') 
     lbl.textContent= label
     lbl.setAttribute('class', 'mx-1 mt-1')
-wrap.append(lbl)
-wrap.append(el)
-wrap.append(del)
-document.querySelector("#elements").append(wrap)
-id++
+    wrap.append(lbl)
+    wrap.append(el)
+    wrap.append(del)
+    document.querySelector("#elements").append(wrap)
+    id++
 }
 
 const appendRadio = (label = "No label", placeholder = "No placeholder") => {
-    let el = document.createElement('input')
 
-    el.textContent = label
-    el.setAttribute('class', 'mt-1')
-    el.setAttribute('placeholder', placeholder)
-    el.setAttribute('type', 'radio')
+    let wrap = document.createElement('div')
+     wrap.setAttribute('id',id)
+     let del = document.createElement('button')
+     del.setAttribute('onclick','document.getElementById("'+id+'").style.display="none"')
+     del.textContent="delete"
+     del.setAttribute('class','btn btn-primary')
+ 
+     let el = document.createElement('input')
+     el.setAttribute('class', ' mt-1')
+     el.setAttribute('placeholder', placeholder)
+     el.setAttribute('type','radio')
+     let lbl=document.createElement('label')
+     lbl.textContent =label
+     lbl.textContent ="you have inserted radio"
+     lbl.setAttribute('class','mx-1 mt-1')
+   
+     wrap.append(el,lbl,del)
+     document.querySelector("#elements").append(wrap) 
+     id++
+ }
 
-    document.querySelector("#elements").append(el) 
-}
+ const appendButton = (label = "No label", placeholder = "No placeholder") => {
+ 
+    let wrap = document.createElement('div')
+     wrap.setAttribute('id',id)
+     let del = document.createElement('button')
+     del.setAttribute('onclick','document.getElementById("'+id+'").style.display="none"')
+     del.textContent="delete"
+     del.setAttribute('class','btn btn-primary')
+ 
+     let el = document.createElement('button')
+     el.setAttribute('class', 'btn btn-primary')
+     el.setAttribute('placeholder', placeholder)
+     el.setAttribute('type','button')
+     el.textContent = label;
+     el.addEventListener('click', function() {
+         alert('inserted button!');
+     });
+     let lbl = document.createElement('label');
+     lbl.textContent = "You have inserted a button";
+   
+   
+     wrap.append(el,lbl,del)
+     document.querySelector("#elements").append(wrap) 
+     id++
+ }
 
